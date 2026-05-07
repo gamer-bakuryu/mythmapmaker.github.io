9b85459ba3bf8af8c28d703860a685f77c3048d0
GAMER-BAKURYU <gamerbakuryu@gmail.com>
2026-05-07 02:22:05 -0300

    Create HierarchyPanel.jsx

---
+++ src/editor/hierarchy/HierarchyPanel.jsx
@@ -0,0 +1,20 @@
+export default function HierarchyPanel(){
+
+  return (
+    <div className="w-72 bg-[#10131a] border-r border-cyan-500/20 p-4 overflow-auto">
+
+      <h2 className="text-xl font-bold mb-4">
+        Hierarchy
+      </h2>
+
+      <div className="space-y-2">
+
+        <div className="bg-black/20 p-2 rounded">
+          Terrain
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
