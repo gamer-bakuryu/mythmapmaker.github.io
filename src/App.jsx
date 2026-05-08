734354231d59a2450605dbabfcf5dcacea8b7dd4
GAMER-BAKURYU <gamerbakuryu@gmail.com>
2026-05-08 04:27:24 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Sidebar from "./components/Sidebar";
+import Toolbar from "./components/Toolbar";
+import CanvasArea from "./components/CanvasArea";
+import LayersPanel from "./components/LayersPanel";
+import StatusBar from "./components/StatusBar";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Toolbar />
+
+      <div className="main-layout">
+        <Sidebar />
+        <CanvasArea />
+        <LayersPanel />
+      </div>
+
+      <StatusBar />
+    </div>
+  );
+}
+
+export default App;
