20149380241a4aa34187c58c314767623244a044
GAMER-BAKURYU <gamerbakuryu@gmail.com>
2026-05-08 05:43:48 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
 
 import "./styles/global.css";
 import "./styles/layout.css";
+import "./styles/sidebar.css";
+import "./styles/canvas.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
