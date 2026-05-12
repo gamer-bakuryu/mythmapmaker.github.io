969a4753449d297d23546cab14d3fe0c1617fc26
GAMER-BAKURYU <gamerbakuryu@gmail.com>
2026-05-12 04:33:49 -0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,25 @@
 function Sidebar() {
   return (
     <aside className="sidebar">
+
       <h2>Ferramentas</h2>
+
+      <button>Novo Mapa</button>
+
+      <button>Upload Sprite</button>
+
+      <button>Terreno</button>
+
+      <button>Linha</button>
+
+      <button>Forma</button>
+
+      <button>Salvar</button>
+
+      <button>Exportar</button>
+
+      <button>Gerar Mapa</button>
+
     </aside>
   );
 }
