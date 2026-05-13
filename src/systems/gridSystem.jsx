1b8180b94757e1c0d06ff68caa1062159c2138af
GAMER-BAKURYU <gamerbakuryu@gmail.com>
2026-05-13 00:40:47 -0300

    Update gridSystem.jsx

--- src/systems/gridSystem.jsx
+++ src/systems/gridSystem.jsx
@@ -1,13 +1,3 @@
-import { Grid } from '@react-three/drei'
-
 export default function GridSystem() {
-  return (
-    <Grid
-      args={[5000, 200]}
-      sectionColor="#00ffff"
-      cellColor="#333333"
-      fadeDistance={3000}
-      fadeStrength={1}
-    />
-  )
+  return null
 }
