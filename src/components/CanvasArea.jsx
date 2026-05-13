a2a26cfa4b44860c0cf34849b123d08ee4481d81
GAMER-BAKURYU <gamerbakuryu@gmail.com>
2026-05-13 00:55:00 -0300

    Update CanvasArea.jsx

--- src/components/CanvasArea.jsx
+++ src/components/CanvasArea.jsx
@@ -1,22 +1,14 @@
 import React from "react";
-
 import { CanvasSystem } from "../systems/canvasSystem";
 import GridSystem from "../systems/gridSystem";
 
-function CanvasArea({
-  children
-}) {
+function CanvasArea({ children }) {
   return (
     <div className="w-full h-full relative">
-
       <CanvasSystem>
-
         <GridSystem />
-
         {children}
-
       </CanvasSystem>
-
     </div>
   );
 }
