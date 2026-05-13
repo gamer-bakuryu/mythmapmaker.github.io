c981d1067870e9b76d04348a35aa1a9839268345
GAMER-BAKURYU <gamerbakuryu@gmail.com>
2026-05-13 02:55:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-<CanvasArea
-  layers={layerSystem.layers}
+import "./styles/global.css";
+import "./styles/layout.css";
+import "./styles/sidebar.css";
+import "./styles/canvas.css";
 
-  activeLayerId={
-    layerSystem.activeLayerId
-  }
+import Toolbar from "./components/Toolbar";
+import Sidebar from "./components/Sidebar";
+import CanvasArea from "./components/CanvasArea";
+import LayersPanel from "./components/LayersPanel";
+import StatusBar from "./components/StatusBar";
 
-  addObjectToLayer={
-    layerSystem.addObjectToLayer
-  }
+import { useLayers } from "./hooks/useLayers";
+import { useAssets } from "./hooks/useAssets";
 
-  selectedObjects={
-    layerSystem.selectedObjects
-  }
+function App() {
 
-  setSelectedObjects={
-    layerSystem.setSelectedObjects
-  }
+  const layerSystem =
+    useLayers();
 
-  updateObject={
-    layerSystem.updateObject
-  }
+  const assetSystem =
+    useAssets();
 
-  deleteSelected={
-    layerSystem.deleteSelected
-  }
+  return (
+    <div className="app-shell">
 
-  duplicateSelected={
-    layerSystem.duplicateSelected
-  }
+      <Toolbar />
 
-  moveSelected={
-    layerSystem.moveSelected
-  }
-/>
+      <div className="editor-shell">
+
+        <Sidebar
+          assets={assetSystem.assets}
+          uploadAssets={
+            assetSystem.uploadAssets
+          }
+        />
+
+        <CanvasArea
+          layers={layerSystem.layers}
+
+          activeLayerId={
+            layerSystem.activeLayerId
+          }
+
+          addObjectToLayer={
+            layerSystem.addObjectToLayer
+          }
+
+          selectedObjects={
+            layerSystem.selectedObjects
+          }
+
+          setSelectedObjects={
+            layerSystem.setSelectedObjects
+          }
+
+          updateObject={
+            layerSystem.updateObject
+          }
+
+          deleteSelected={
+            layerSystem.deleteSelected
+          }
+
+          duplicateSelected={
+            layerSystem.duplicateSelected
+          }
+
+          moveSelected={
+            layerSystem.moveSelected
+          }
+        />
+
+        <LayersPanel
+          {...layerSystem}
+        />
+
+      </div>
+
+      <StatusBar />
+
+    </div>
+  );
+}
+
+export default App;
